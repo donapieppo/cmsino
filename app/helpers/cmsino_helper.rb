14ae931335bd4441d054d0b1af42d40916622144
Pietro Donatini <pietro.donatini@unibo.it>
2013-01-16 23:43:12 +0100

    rename page variable

--- app/helpers/cmsino_helper.rb
+++ app/helpers/cmsino_helper.rb
@@ -1,8 +1,8 @@
 module CmsinoHelper
 
-  # in controller and view you have now @page
+  # in controller and view you have now @cmsino_page
   def editable_page(name)
-    @page = Cmsino::Page.new(name)
+    @cmsino_page = Cmsino::Page.new(name)
   end
 
   # display the 'edit' link if user has the rights
@@ -17,20 +17,20 @@ module CmsinoHelper
   # display the current content. Eventually creates it if content with the
   # name doesen't exists. 
   def editable_content(name)
-    if ! @page
-      raise "FIXME trovare page da snippet name quando possibile"
+    if ! @cmsino_page
+      raise "FIXME trovare cmsino_page da snippet name quando possibile"
     end
-    content = Cmsino::Content.find_or_create_by_page_and_name(@page.name, name)
+    content = Cmsino::Content.find_or_create_by_page_and_name(@cmsino_page.name, name)
     raw %Q|<div id="#{content.div_id}">
 #{content.text}#{editable_content_link(content)}
 </div>|
   end
 
   def editable_image_content(name)
-    if ! @page
-      raise "FIXME trovare page da snippet name quando possibile"
+    if ! @cmsino_page
+      raise "FIXME trovare cmsino_page da snippet name quando possibile"
     end
-    content = Cmsino::ImageContent.find_or_create_by_page_and_name(@page.name, name)
+    content = Cmsino::ImageContent.find_or_create_by_page_and_name(@cmsino_page.name, name)
     raw %Q|<h4>#{content.title}</h4>
 #{content.text}#{editable_content_link(content)}|
   end
