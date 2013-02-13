e01a72ccac8d8a6c3c3477e541e94b06228ee04a
Pietro Donatini <pietro.donatini@unibo.it>
2013-02-13 17:50:18 +0100

    added locale

--- db/migrate/2013011001_create_cmsino_content_schema.rb
+++ db/migrate/2013011001_create_cmsino_content_schema.rb
@@ -1,8 +1,12 @@
 class CreateCmsinoContentSchema < ActiveRecord::Migration
   def up
+    # page = :home 
+    # name = :index
+    # locale = :it
     create_table :cmsino_contents do |t|
       t.string   :page
       t.string   :name
+      t.string   :locale
       t.string   :title
       t.text     :text
       t.string   :type
