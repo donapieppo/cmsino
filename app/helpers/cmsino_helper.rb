c69981410c2c12c72455e850f343fd5197bbaa62
Pietro Donatini <pietro.donatini@unibo.it>
2013-02-13 17:49:45 +0100

    added locale

--- app/helpers/cmsino_helper.rb
+++ app/helpers/cmsino_helper.rb
@@ -20,7 +20,7 @@ module CmsinoHelper
     if ! @cmsino_page
       raise "FIXME trovare cmsino_page da snippet name quando possibile"
     end
-    content = Cmsino::Content.find_or_create_by_page_and_name(@cmsino_page.name, name)
+    content = Cmsino::Content.find_or_create_by_page_and_name_and_locale(@cmsino_page.name, name, I18n.locale)
     raw %Q|<div id="#{content.div_id}">
 #{content.text}#{editable_content_link(content)}
 </div>|
@@ -30,7 +30,7 @@ module CmsinoHelper
     if ! @cmsino_page
       raise "FIXME trovare cmsino_page da snippet name quando possibile"
     end
-    content = Cmsino::ImageContent.find_or_create_by_page_and_name(@cmsino_page.name, name)
+    content = Cmsino::ImageContent.find_or_create_by_page_and_name_and_locale(@cmsino_page.name, name, I18n.locale)
     raw %Q|<h4>#{content.title}</h4>
 #{content.text}#{editable_content_link(content)}|
   end
