b3de5ccdbb82d2da372b2c9d79cb1355d4363f1a
Pietro Donatini <pietro.donatini@unibo.it>
2013-03-04 17:04:39 +0100

    validate uniqueness

--- app/models/cmsino/content.rb
+++ app/models/cmsino/content.rb
@@ -5,6 +5,8 @@ class Cmsino::Content < ActiveRecord::Base
 
   validates_presence_of :page, :name, :locale
 
+  validates_uniqueness_of :name, :scope => [:page, :locale]
+
   def div_id
     "content_#{@page}_#{@name}"
   end
