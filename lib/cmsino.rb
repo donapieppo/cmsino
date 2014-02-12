497b25aefcdc999adc84c0556e25fe1d87c836e4
Pietro Donatini <pietro.donatini@unibo.it>
2014-02-12 11:07:13 +0100

    for spec and isolate_namespace

--- lib/cmsino.rb
+++ lib/cmsino.rb
@@ -2,5 +2,11 @@ require "cmsino/version"
 
 module Cmsino
   class Engine < Rails::Engine
+    isolate_namespace Cmsino
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
