50b9d6a257001c6c39dbbdca55eefa8914e6e466
Donapieppo <donapieppo@yahoo.it>
2014-02-13 10:12:32 +0100

    configuration

--- lib/cmsino/conf.rb
+++ lib/cmsino/conf.rb
@@ -7,13 +7,13 @@ module Cmsino
     attr_accessor :locales
     attr_accessor :pages
 
-    def self.setup(&block)
-      block.call Conf.instance
-    end
-
     def initialize
       @locales = ['it']
       @pages = YAML.load_file(File.join(Rails.root, "config", "cmsino.yml"))
+    end
+
+    def self.setup(&block)
+      block.call Conf.instance
     end
 
     def page_description(name)
