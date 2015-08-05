11a126055baf4c2a4d9c9373cf696af774ab6077
Pietro Donatini <pietro.donatini@unibo.it>
2015-08-05 16:43:21 +0200

    inspied by wordpress database new db fields. Page becames umbrella

--- db/migrate/2013011001_create_cmsino_content_schema.rb
+++ db/migrate/2013011001_create_cmsino_content_schema.rb
@@ -1,19 +1,29 @@
 class CreateCmsinoContentSchema < ActiveRecord::Migration
   def up
-    # page = :home 
+    # umbrella = :home 
     # name = :index
     # locale = :it
     create_table :cmsino_contents do |t|
-      t.string   :page
+      t.string   :umbrella
       t.string   :name
       t.string   :locale
       t.string   :title
       t.text     :text
+      t.text     :excerpt  
+      t.datetime :date
       t.datetime :from
       t.datetime :to
+      t.string   :type
+      t.string   :status 
       t.string   :type
       t.timestamps
     end
   end
 end
+
+# post_name             | varchar(200)        | NO   | MUL |                     |                |
+# post_parent           | bigint(20) unsigned | NO   | MUL | 0                   |                |
+# post_type             | varchar(20)         | NO   | MUL | post                |                |
+# post_mime_type        | varchar(100)        | NO   |     |                     |                |
+
 
