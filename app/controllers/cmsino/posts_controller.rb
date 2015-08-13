3ac7cccf2df3a0afd12daef0069bf5f8e2b10f73
Pietro Donatini <pietro.donatini@unibo.it>
2015-08-13 16:30:28 +0200

    typo

--- app/controllers/cmsino/posts_controller.rb
+++ app/controllers/cmsino/posts_controller.rb
@@ -1,4 +1,4 @@
-class Cmsino::PostsController < Cmsino::ContentsController
+class Cmsino::PostsController < Cmsino::ContentsController
   layout 'cmsino/layouts/cmsino'
   authorize_resource :class => Cmsino::Content
 
