443f6f62f5bddd4fefc18a41cfcb631f28f21dca
Pietro <donapieppo@yahoo.it>
2017-06-27 15:41:54 +0200

    users

---
+++ db/migrate/2017061301_create_cmsino_users_schema.rb
@@ -0,0 +1,15 @@
+class CreateCmsinoMediaUsesSchema < ActiveRecord::Migration[5.0]
+  def up
+    create_table :cmsino_media_uses do |t|
+      t.integer :cmsino_content_id
+      t.integer :cmsino_medium_id
+    end
+  end
+
+  def self.down
+    remove_table :cmsino_media_uses
+  end
+end
+
+
+
